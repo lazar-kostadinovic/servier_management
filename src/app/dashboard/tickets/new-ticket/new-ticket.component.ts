b11f4aa8fdc0dd6630dfa497730fbb2af8b0928b
Lazar Kostadinovic <kostadinovicl999@gmail.com>
2024-11-09 03:52:55 +0100

    Update status and new-ticket component

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,15 +1,25 @@
-import { Component, input } from '@angular/core';
+import { Component, ElementRef, input, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
-import { ControlComponent } from "../../../shared/control/control.component";
+import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent,ControlComponent],
+  imports: [ButtonComponent, ControlComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
 
-
+  onSubmit(
+    titleInput: HTMLInputElement,
+    textInput: string
+    // form: HTMLFormElement
+  ) {
+    console.dir(titleInput.value);
+    console.dir(textInput);
+    this.form?.nativeElement. reset();
+  }
 }
--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-status',
   standalone: true,
   imports: [],
   templateUrl: './status.component.html',
-  styleUrl: './status.component.css'
+  styleUrl: './status.component.css',
 })
-export class StatusComponent {
-  currentStatus = 'online';
+export class StatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  // private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef);
+  constructor() {}
+  ngOnInit() {
+    console.log('ON INIT');
+    // this.interval = setInterval(() => {
+    const interval = setInterval(() => {
+      const rnd = Math.random();
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
+  }
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+  }
+  // ngOnDestory() {
+  //   clearTimeout(this.interval);
+  // }
 }
